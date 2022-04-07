4be6795bc4533cc5f326afc2a644fc98a13d4331
Justin <jingyu120@gmail.com>
2022-04-07 12:49:50 -0500

    finish writing tests for header componenent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,15 +17,15 @@ const Header: FunctionComponent<Props> = (props) => {
     <div className="flex w-full my-10 justify-between items-center px-3">
       {modal && <Modal toggleModal={toggleModal} modal={modal} />}
       <div>
-        <img className="h-7" src={Logo} alt="logo" />
+        <img data-testid="logo" className="h-7" src={Logo} alt="logo" />
       </div>
       <div className="flex items-center" onClick={toggleModal}>
         <span className="shadow p-3 rounded-l-2xl w-[250px] pl-5">
           Helsinki Finland
         </span>
         <span className="shadow p-3 text-gray-300 w-[200px]">Add guests</span>
-        <span className="flex justify-center items-center shadow h-[48px] w-[48px] rounded-r-2xl">
-          <SearchIcon className="w-5 text-red-500" />
+        <span  className="flex justify-center items-center shadow h-[48px] w-[48px] rounded-r-2xl">
+          <SearchIcon data-testid="search-icon" className="w-5 text-red-500" />
         </span>
       </div>
     </div>
--- src/components/__test__/Header.test.tsx
+++ src/components/__test__/Header.test.tsx
@@ -3,12 +3,27 @@ import {render, screen} from "@testing-library/react";
 import Header from "../Header";
 
 describe("Header", () => {
-    beforeAll(() => {
+    beforeEach(() => {
         render(<Header />)
     })
     test("test helsinki", () => {
         const helsinkiElement = screen.getByText(/Helsinki/i);
 
         expect(helsinkiElement).toBeInTheDocument()
+    })
+    test("test add guest displayed", () => {
+        const addGuestText = screen.getByText(/Add guests/i);
+
+        expect(addGuestText).toBeInTheDocument()
+    })
+    test("test logo img displayed", () => {
+        const logoElement = screen.getByTestId("logo")
+
+        expect(logoElement).toBeInTheDocument()
+    })
+    test("test search icon displayed", () => {
+        const searchIconElement = screen.getByTestId("search-icon")
+
+        expect(searchIconElement).toBeInTheDocument()
     })
 })
